1609480a8b0fae15be4ee25a6feedad30d2a9f56
Jay20241 <167299716+Jay20241@users.noreply.github.com>
2025-01-01 13:41:26 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //Treat all JS code as newer version. //it is auto, not need to write
+
+//alert("Hello");
+//alert(3+3) //we are using node.js, not browser
+
+let name = "jay"
+let age = 20
+let isLoggedIn = false
+let state = null;
+
+//Primitive dataTypes
+//number => 2 to power 53
+//bigint
+//string => "" or ''
+//boolean
+//null => standalone value and it is one dataType
+//undefined 
+//symbol => it is used for uniqueness, used in Figma and React
+
+//object
+
+console.log(typeof "jay");//It will show "string" in console
+console.log(typeof age); //It will show "int" in console
+console.log(typeof null); //It will show "object" in console
+console.log(typeof undefined);//It will show "undefined" in console
