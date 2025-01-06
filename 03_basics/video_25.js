8bd5bafa6b5b75a776899c0b651ede793c911997
Jay20241 <167299716+Jay20241@users.noreply.github.com>
2025-01-06 12:32:20 +0000

    call stack

---
+++ 03_basics/video_25.js
@@ -0,0 +1,20 @@
+//Call stack
+//JavaScript Execution Context(J EC)
+
+//JavaScript is single threaded.
+
+//[Code file {}] => Global EC - this keyword => For browser it returns WINDOW Object.
+//Different this for node, deno, bun 
+
+//Three types:
+/**
+ * Global Execution Context
+ * Function Execution Context
+ * Eval Execution Context, which is property of Global EC
+ */
+
+/**
+ * [Code File {}] => 
+ *                  1. Memory Creation Phase
+ *                  2. Execution Phase
+ */
