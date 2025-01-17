c6e1f7dd01c6e9f7d22af32660e304bfb95fc573
Jay20241 <167299716+Jay20241@users.noreply.github.com>
2025-01-17 13:44:23 +0000

    filter, map, reduce

--- advance/fliterMap.js
+++ advance/fliterMap.js
@@ -33,10 +33,46 @@ myNums.forEach((num)=>{
 const books = [
     {
         title: 'Rich Dad',
-        genre: 'Finance'
+        genre: 'Finance',
+        publish: 2002
     },
     {
         title: 'Chankya',
-        genre: 'History'
+        genre: 'History',
+        publish: 1800
     }
-]
+];
+
+let userbook = books.filter((bk) => bk.genre === 'History');
+
+userbook = books.filter((bk) => bk.publish >= 2000 && bk.genre === "Finance");
+
+//---------------------map method-------------------------//
+
+const myNumbers = [1,2,3,4,5,6];
+const add10 = myNumbers.map( (num) => num+10);
+console.log(add10); //[11,12,13,14,15,16]
+
+//Chaining :
+/*
+const add10 = myNumbers
+                    .map()
+                    .map()
+                    .filter();
+
+*/
+
+//-----------reduce method (used in shopping cart etc) see mdn Docs ----------//
+
+const myNumbers2 = [1,2,3];
+
+
+// const myTotal = myNumbers2.reduce(function (acc,current){
+//     console.log(`acc: ${acc} and curr = ${current}`);
+//     return acc + current
+// }, 0);
+
+const myTotal = myNumbers2.reduce((acc,current) => acc + current, 0);
+
+console.log(myTotal);//6
+//You can use this in Array of Objects.
